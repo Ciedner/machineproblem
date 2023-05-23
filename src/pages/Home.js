c9bea321ddbf8273d4a4f35767e0783971e3eea9
Ciedner <ciednermabale.com>
2023-05-23 08:57:45 +0800

    useState & effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Navbar, Nav } from 'react-bootstrap';
+import axios from 'axios';
+import { useState } from 'react';
 
 function HomePage() {
+  const [entry, setEntry] = useState([]);
+  useEffect(() => {
+    axios.get('https://api.publicapis.org/entries')
+    .then(response => {
+      console.log(response.data.entries);
+      setEntry(response.data.entries);
+    });
+  },[])
+
+  
   const backgroundStyle = {
     backgroundImage: 'url("https://images.unsplash.com/photo-1564507555469-a3772d88d2fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80")',
     backgroundSize: 'cover',
@@ -16,6 +28,7 @@ function HomePage() {
   };
 
   return (
+    
     <Container fluid style={backgroundStyle}>
       <Row className="d-flex flex-column justify-content-center align-items-center text-center" style={{ height: '100vh' }}>
         <Col md={{ span: 8, offset: 2 }}>
@@ -29,5 +42,4 @@ function HomePage() {
     </Container>
   );
 }
-
 export default HomePage;
